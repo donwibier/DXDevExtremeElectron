aaaf11577ab9da9e47817fe71c2ec3b2b8fcccf2
Don Wibier <don@anykey.nl>
2016-08-01 18:55:26 +0200

    Sql based datasource incl sorting and filtering

--- js/db.js
+++ js/db.js
@@ -1,7 +1,10 @@
 var fs = require('fs');
 var path = require('path');
+//var dx = require(path.join(process.cwd(), 'node_modules/devextreme/dist/js/dx.all.js'));
+
 
 function db() {
+
     this.config = JSON.parse(fs.readFileSync(path.join(process.cwd(), 'app.json')));
     var databaseFile = (this.config.database.split('\\')).length == 1 ? path.join(process.cwd(), 'model', this.config.database + '.db') : this.config.database;
     if (this.config.cipher) {
@@ -26,23 +29,113 @@ function db() {
 db.prototype.database = null;
 db.prototype.config = null;
 
-db.prototype.getTasks = function() {
-    var result = this.database.run("SELECT id, duedate, priority, description FROM todo");
-    return result;
-}
+db.prototype.Tasks = function() {
 
-db.prototype.insertTask = function(t) {
-    return this.database.run("INSERT INTO todo (duedate, priority, description) VALUES (?,?,?)", [t.duedate, t.priority, t.task, t.id]);
-}
+    var me = this,
+        options = new DevExpress.data.CustomStore({
+            load: function(options) {
+                //debugger;
+                var d = $.Deferred(),
+                    sql = ["SELECT *", "FROM todo"];
 
-db.prototype.updateTask = function(t) {
-    this.database.update("UPDATE todo SET duedate = ?, priority = ?, description = ? WHERE id = ?", [t.duedate, t.priority, t.task, t.id]);
-}
+                /*TODO: Add sql options */
+                if (options.filter && options.filter.length > 0) {
+                    sql.push("WHERE " + options.filter.map(function(c, i, a) {
+                        if (typeof c === "object" && c.length) {
+                            return (typeof c[2] === 'string') ? c[0] + " " + c[1] + " '" + c[2] + "'" : c.join(" ");
+                        } else {
+                            return c;
+                        }
+                    }).join(" "));
+                }
 
-db.prototype.updateTask = function(t) {
-    this.database.run("DELETE FROM todo WHERE id = ?", [t.id]);
-}
+                if (options.sort && options.sort.length > 0) {
+                    sql.push("ORDER BY " + options.sort.map(function(c, i, a) {
+                        return c.selector + " COLLATE NOCASE" + (c.desc ? " DESC" : "");
+                    }));
+                }
+                if (options.take > 0)
+                    sql.push("LIMIT " + options.take);
+                if (options.skip > 0)
+                    sql.push("OFFSET " + options.skip);
 
+                me.database.runAsync(sql.join(" "),
+                    function(rows) {
+                        d.resolve(rows);
+                    });
+                return d.promise();
+            },
+            totalCount: function(options) {
+                var d = $.Deferred(),
+                    sql = ["SELECT *", "FROM todo"];
+                /*TODO: Add sql options */
+                if (options.filter && options.filter.length > 0) {
+                    sql.push("WHERE " + options.filter.map(function(c, i, a) {
+                        if (typeof c === "object" && c.length) {
+                            return (typeof c[2] === 'string') ? c[0] + " " + c[1] + " '" + c[2] + "'" : c.join(" ");
+                        } else {
+                            return c;
+                        }
+                    }).join(" "));
+                }
+                me.database.runAsync(sql.join(" "),
+                    function(rows) {
+                        d.resolve(rows);
+                    });
+                return d.promise();
+            },
+            byKey: function(key) {
+                var d = $.Deferred(),
+                    sql = "SELECT * FROM todo WHERE id = " + key;
+                me.database.runAsync(sql,
+                    function(rows) {
+                        d.resolve(rows);
+                    });
+                return d.promise();
+            },
+            insert: function(values) {
+                var d = $.Deferred();
+                me.database.insert("todo", {
+                        duedate: values.duedate,
+                        priority: values.priority,
+                        description: values.description
+                    },
+                    function(res) {
+                        console.log(res);
+                        if (res.error)
+                            throw res.error;
+                        d.resolve(me.database.run("SELECT * FROM todo WHERE id = ?", res));
+                    });
+                return d.promise();
+            },
+            update: function(key, values) {
+                var d = $.Deferred();
+                me.database.update("todo", {
+                        duedate: values.duedate,
+                        priority: values.priority,
+                        description: values.description
+                    }, key,
+                    function(res) {
+                        console.log(res);
+                        if (res.error)
+                            throw res.error;
+                        d.resolve(me.database.run("SELECT * FROM todo WHERE id = ?", res));
+                    });
+                return d.promise();
+            },
+            remove: function(key) {
+                var d = $.Deferred(),
+                    sql = "DELETE * FROM todo WHERE id = " + key;
+                me.database.runAsync(sql,
+                    function(id) {
+                        d.resolve(id);
+                    });
+                return d.promise();
+            }
+        });
+
+    return new DevExpress.data.DataSource(options);
+}
 
 // Exporting module
 module.exports = new db();
--- views/tasks.js
+++ views/tasks.js
@@ -1,65 +1,56 @@
 dxdon.Tasks = function(params, viewInfo) {
     "use strict";
-    var path = require('path');
-    var d = require(path.join(process.cwd(), 'js/db.js'));
-    // var shouldReload = false,
-    //     openCreateViewAsRoot = viewInfo.layoutController.name === "split",
-    //     isReady = $.Deferred(),
-    //     dataSourceObservable = ko.observable(),
-    //     dataSource;
+    var path = require('path'),
+        d = require(path.join(process.cwd(), 'js/db.js')),
+        openCreateViewAsRoot = viewInfo.layoutController.name === "split",
+        dataSource = d.Tasks();
 
-    // function handleTaskModification() {
-    //     shouldReload = true;
-    // }
+    function viewShownEvent() {
+        dataSource.load();
+    }
 
-    // function handleViewShowing() {
-    //     if(!dataSourceObservable()) {
-    //         dataSourceObservable(dataSource);
-    //         dataSource.load()
-    //             .fail(function () {
-    //                 shouldReload = true;
-    //             })
-    //             .always(function() {
-    //                 isReady.resolve();
-    //             });
-    //     }
-    //     else if(shouldReload) {
-    //         refreshList();
-    //     }
-    // }
-
-    // function handleViewDisposing() {
-    //     Application1.db.Customer.off("modified", handleTaskModification);
-    // }
-
-    // function refreshList() {
-    //     shouldReload = false;
-    //     dataSource.pageIndex(0);
-    //     dataSource.load();
-    // }
-
-    // dataSource = new DevExpress.data.DataSource({
-    //     store: db.getTasks(),
-    //     map: function(item) {
-    //         return new dxdon.CustomerViewModel(item);
-    //     }
-    // });
-
-    //Application1.db.Customer.on("modified", handleTaskModification);
-
-
-
-    // return {
-    //     isReady: isReady.promise(),
-    //     dataSource: d.getTasks(),
-    //     refreshList: refreshList,
-    //     viewShowing: handleViewShowing,
-    //     viewDisposing: handleViewDisposing,
-    //     openCreateViewAsRoot: openCreateViewAsRoot
-    // };
     // need to do this async
     return {
-        dataSource: d.getTasks(),
-        isReady: true
+        dataSource: dataSource,
+        openCreateViewAsRoot: openCreateViewAsRoot,
+        viewShown: viewShownEvent,
+        refreshList: viewShownEvent,
+        dataGridOptions: {
+            dataSource: dataSource,
+            paging: {
+                pageSize: 10
+            },
+            editing: {
+                mode: "row",
+                allowUpdating: true,
+                allowDeleting: true,
+                allowAdding: true
+            },
+            columns: [{
+                dataField: 'duedate',
+                caption: 'Due Date',
+                dataType: 'date'
+            }, {
+                dataField: 'priority',
+                caption: 'Priority',
+                dataType: 'number'
+            }, {
+                dataField: 'description',
+                caption: 'Tasks',
+                dataType: 'string'
+            }],
+            filterRow: {
+                visible: true,
+                applyFilter: "auto"
+            },
+            headerFilter: {
+                visible: true
+            },
+            searchPanel: {
+                visible: true,
+                width: 240,
+                placeholder: "Search..."
+            }
+        }
     }
 };
